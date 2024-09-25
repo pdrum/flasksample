04c4182d8548d3b6ea7bacc1de26d1cef2f77ed6
Pedram Hajesmaeeli <pedram.esmaeeli@gmail.com>
2024-09-25 14:36:48 +0200

    Complete the service layer

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,11 +1,18 @@
 from flask import Blueprint, request, jsonify
-from app.services.user_service import register_user, AuthAttempt
+from app.services.user_service import register_user, AuthAttempt, login
 
 auth_bp = Blueprint('auth', __name__)
 
-
 @auth_bp.route('/register', methods=['POST'])
-def register():
+def register_api():
     data = request.get_json()
     result = register_user(AuthAttempt(username=data.get('username'), password=data.get('password')))
     return jsonify({"token": result.token}), 201
+
+
+@auth_bp.route('/login', methods=['POST'])
+def login_api():
+    data = request.json
+    result = login(AuthAttempt(username=data['username'], password=data['password']))
+    return jsonify({"token": result.token}), 200
+
