f69056e6f7909be562da0a1b330be54c2b4c8cc9
Pedram Hajesmaeeli <pedram.esmaeeli@gmail.com>
2024-09-25 13:59:40 +0200

    Create model and migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 # app/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
@@ -12,6 +13,10 @@ def create_app():
 
     # Initialize extensions
     db.init_app(app)
+    Migrate(app, db)
+
+    # Import and register models (this is key for migrations)
+    from app.models import User
 
     # Register Blueprints (for routes)
     from app.routes.hello import hello_bp
